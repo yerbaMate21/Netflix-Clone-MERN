4f7636d7e3ebe780bc488dcccd1449d074d3627c
Rafal Sereda <seredarafal@gmail.com>
2024-02-04 21:27:51 +0100

    pdate

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -20,30 +20,29 @@ export const useLogin = () => {
       password: null,
     });
 
-    try {
-      const response = await fetch("/api/user/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
-      const json = await response.json();
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await response.json();
 
-      if (!response.ok) {
-        setIsLoading(false);
-        setError({
-          ...error,
-          email: json.emailError,
-          password: json.passwordError,
-        });
-      }
+    if (!response.ok) {
+      setIsLoading(false);
+      setError({
+        ...error,
+        email: json.emailError,
+        password: json.passwordError,
+      });
+    }
+
+    if (response.ok) {
       localStorage.setItem("user", JSON.stringify(json));
 
       dispatch({ type: "LOGIN", payload: json });
 
       setIsLoading(false);
       navigate("/");
-    } catch (error) {
-      console.log(error);
     }
   };
 
